8aab8b59e23ed696a6d38693e7c1c046c96b67b3
Jungkook Lee <jungkook@amazon.com>
2022-05-24 18:39:10 +0000

    Fix MVU failure regarding runtime_error function
    T-SQL allows an empty/space-only string as a default constraint of
    NUMERIC column in CREATE TABLE statement. However, it will eventually
    throw an error when actual INSERT happens for the default value.
    
    To support this behavior, we use a function
    sys.babelfish_runtime_error(), which raises an error in execution time.
    
    However, pg_dump evaluates the runtime error function and replaces it
    with an error string that causes MVU failure during restore. This commit
    replaces the error string by sys.babelfish_runtime_error() again.
    
    Task: BABEL-3234
    Signed-off-by: Jungkook Lee <jungkook@amazon.com>

--- src/bin/pg_dump/dump_babel_utils.h
+++ src/bin/pg_dump/dump_babel_utils.h
@@ -22,6 +22,7 @@
 
 
 extern void bbf_selectDumpableCast(CastInfo *cast);
+extern char *getModifiedDefaultExpr(Archive *fout, const AttrDefInfo attrDefInfo);
 extern bool isBabelfishDatabase(Archive *fout);
 extern void fixTsqlTableTypeDependency(Archive *fout, DumpableObject *func, DumpableObject *tabletype, char deptype);
 extern bool isTsqlTableType(Archive *fout, const TableInfo *tbinfo);
