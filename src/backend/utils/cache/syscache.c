sanchitachaurasia/DBIS-Project

Static assertions cleanup
Because we added StaticAssertStmt() first before StaticAssertDecl(),
some uses as well as the instructions in c.h are now a bit backwards
from the "native" way static assertions are meant to be used in C.
This updates the guidance and moves some static assertions to better
places.

Specifically, since the addition of StaticAssertDecl(), we can put
static assertions at the file level.  This moves a number of static
assertions out of function bodies, where they might have been stuck
out of necessity, to perhaps better places at the file level or in
header files.

Also, when the static assertion appears in a position where a
declaration is allowed, then using StaticAssertDecl() is more native
than StaticAssertStmt().

Reviewed-by: John Naylor <john.naylor@enterprisedb.com>
Discussion: https://www.postgresql.org/message-id/flat/941a04e7-dd6f-c0e4-8cdf-a33b3338cbda%40enterprisedb.com

src/backend/utils/cache/syscache.c
@@ -1040,6 +1040,9 @@ static const struct cachedesc cacheinfo[] = {
 	}
 };
 
+StaticAssertDecl(lengthof(cacheinfo) == SysCacheSize,
+				 "SysCacheSize does not match syscache.c's array");
+
 static CatCache *SysCache[SysCacheSize];
 
 static bool CacheInitialized = false;
@@ -1067,9 +1070,6 @@ void
 InitCatalogCache(void)
 {
 	int			cacheId;
-
-	StaticAssertStmt(lengthof(cacheinfo) == SysCacheSize,
-					 "SysCacheSize does not match syscache.c's array");
 
 	Assert(!CacheInitialized);
 

amazon-aurora/postgresql_modified_for_babelfish

BABELFISH-CONFLICT: see Postgres community repo for original commit
(cherry picked from commit 75f49221c22286104f032827359783aa5f4e6646)

src/backend/utils/cache/syscache.c
@@ -1028,6 +1028,9 @@ static const struct cachedesc cacheinfo[] = {
 	}
 };
 
+StaticAssertDecl(lengthof(cacheinfo) == SysCacheNoExtensionSize,
+				 "SysCacheSize does not match syscache.c's array");
+
 static CatCache *SysCache[SysCacheSize];
 
 static bool CacheInitialized = false;
@@ -1055,9 +1058,6 @@ void
 InitCatalogCache(void)
 {
 	int			cacheId;
-
-	StaticAssertStmt(lengthof(cacheinfo) == SysCacheNoExtensionSize,
-					 "SysCacheSize does not match syscache.c's array");
 
 	Assert(!CacheInitialized);
 