6c457843a213dced00727893cbf8b0a17a2ff802
Tanzeel Khan <tzlkhan@amazon.com>
2024-10-21 10:27:01 +0000

    handle dump restore for db_ddladmin
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- src/bin/pg_dump/dumpall_babel_utils.c
+++ src/bin/pg_dump/dumpall_babel_utils.c
@@ -36,9 +36,9 @@ typedef enum {
 static babelfish_status bbf_status = NONE;
 
 static char default_bbf_roles[] = "('sysadmin', 'bbf_role_admin', 'securityadmin', "
-								  "'master_dbo', 'master_db_owner', 'master_guest', 'master_db_accessadmin', "
-								  "'msdb_dbo', 'msdb_db_owner', 'msdb_guest', 'msdb_db_accessadmin', "
-								  "'tempdb_dbo', 'tempdb_db_owner', 'tempdb_guest', 'tempdb_db_accessadmin')";
+								  "'master_dbo', 'master_db_owner', 'master_guest', 'master_db_accessadmin', 'master_db_ddladmin' "
+								  "'msdb_dbo', 'msdb_db_owner', 'msdb_guest', 'msdb_db_accessadmin', 'msdb_db_ddladmin' "
+								  "'tempdb_dbo', 'tempdb_db_owner', 'tempdb_guest', 'tempdb_db_accessadmin', 'tempdb_db_ddladmin')";
 
 /*
  * Run a query, return the results, exit program on failure.
