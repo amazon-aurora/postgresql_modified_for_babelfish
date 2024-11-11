afe4d5a3a0afa2410859f414dad76765cbaba64b
Tanzeel Khan <tzlkhan@amazon.com>
2024-11-11 06:04:03 +0000

    fix
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- src/backend/commands/dropcmds.c
+++ src/backend/commands/dropcmds.c
@@ -109,9 +109,9 @@ RemoveObjects(DropStmt *stmt)
 
 		/* Check permissions. */
 		namespaceId = get_object_namespace(&address);
-		if ((!OidIsValid(namespaceId) ||
-			!object_ownercheck(NamespaceRelationId, namespaceId, GetUserId())) &&
-			!IS_BBF_DB_DDLADMIN(namespaceId))
+		if (!OidIsValid(namespaceId) ||
+			(!object_ownercheck(NamespaceRelationId, namespaceId, GetUserId()) &&
+			!IS_BBF_DB_DDLADMIN(namespaceId)))
 			check_object_ownership(GetUserId(), stmt->removeType, address,
 								   object, relation);
 
