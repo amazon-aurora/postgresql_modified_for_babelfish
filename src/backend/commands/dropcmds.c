27d50149baac7ee4f5eef8f82675ca95f38ccce2
Tanzeel Khan <tzlkhan@amazon.com>
2024-11-12 17:23:55 +0000

    fix some more ddls
    Signed-off-by: Tanzeel Khan <tzlkhan@amazon.com>

--- src/backend/commands/dropcmds.c
+++ src/backend/commands/dropcmds.c
@@ -109,9 +109,10 @@ RemoveObjects(DropStmt *stmt)
 
 		/* Check permissions. */
 		namespaceId = get_object_namespace(&address);
-		if (!OidIsValid(namespaceId) ||
-			(!object_ownercheck(NamespaceRelationId, namespaceId, GetUserId()) &&
-			!IS_BBF_DB_DDLADMIN(namespaceId)))
+		if ((!OidIsValid(namespaceId) ||
+			!object_ownercheck(NamespaceRelationId, namespaceId, GetUserId())) &&
+			(!OidIsValid(namespaceId) || !IS_BBF_DB_DDLADMIN(namespaceId)) &&
+			(relation == NULL || !IS_BBF_DB_DDLADMIN(RelationGetNamespace(relation))))
 			check_object_ownership(GetUserId(), stmt->removeType, address,
 								   object, relation);
 
