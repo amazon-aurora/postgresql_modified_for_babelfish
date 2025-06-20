8e1ff4cd645c89d4800d5bc5ff96205e52d7506e
Shreya Rai <shreyaxr@amazon.com>
2025-06-20 08:36:12 +0000

    fixed indentation
    Signed-off-by: Shreya Rai <shreyaxr@amazon.com>

--- src/backend/catalog/aclchk.c
+++ src/backend/catalog/aclchk.c
@@ -2041,7 +2041,7 @@ ExecGrant_Relation(InternalGrant *istmt)
 			{
 				(*bbf_execute_grantstmt_as_dbsecadmin_hook) (objtype, relOid, ownerId, this_privileges, &grantorId, &avail_goptions);
 			}
-			
+
 			/*
 			 * Restrict the privileges to what we can actually grant, and emit
 			 * the standards-mandated warning and error messages.
@@ -2053,10 +2053,11 @@ ExecGrant_Relation(InternalGrant *istmt)
 										 NameStr(pg_class_tuple->relname),
 										 0, NULL);
 
-			/* Call the hook to add the permission in bbf_schema_permissions catalog 
-			*  If the hook returns false, indicates that object-level and schema-level grants both are present and schema-level grant is revoked.
-			*  In such case we remove schema-level entry from the bbf_schema_permissions catalog but skip the execution of revoke as object-level grants exist.
-			*/
+			/* 
+			 * Call the hook to add the permission in bbf_schema_permissions catalog 
+			 *  If the hook returns false, indicates that object-level and schema-level grants both are present and schema-level grant is revoked.
+			 *  In such case we remove schema-level entry from the bbf_schema_permissions catalog but skip the execution of revoke as object-level grants exist.
+			 */
 			if (update_bbf_schema_permissions_catalog_hook && !(*update_bbf_schema_permissions_catalog_hook) (this_privileges, istmt->is_grant, istmt->grantees,
 				istmt->col_privs, pg_class_tuple->oid, GetUserNameFromId(grantorId, false), 
 				istmt->grant_option, GetUserNameFromId(ownerId, false), istmt->objtype))
@@ -2069,7 +2070,8 @@ ExecGrant_Relation(InternalGrant *istmt)
 				table_close(attRelation, RowExclusiveLock);
 				table_close(relation, RowExclusiveLock);
 				return;
-			}							 
+			}
+										 
 			/*
 			 * Generate new ACL.
 			 */
@@ -2276,7 +2278,6 @@ ExecGrant_common(InternalGrant *istmt, Oid classid, AclMode default_privs,
 			(*bbf_execute_grantstmt_as_dbsecadmin_hook) (get_object_type(classid, objectid), objectid, ownerId, istmt->privileges, &grantorId, &avail_goptions);
 		}
 
-
 		nameDatum = SysCacheGetAttrNotNull(cacheid, tuple,
 										   get_object_attnum_name(classid));
 
@@ -2291,10 +2292,11 @@ ExecGrant_common(InternalGrant *istmt, Oid classid, AclMode default_privs,
 									 NameStr(*DatumGetName(nameDatum)),
 									 0, NULL);
 
-		/* Call the hook to add the permission in bbf_schema_permissions catalog 
-		*  If the hook returns false, indicates that object-level and schema-level grants both are present and schema-level grant is revoked.
-		*  In such case we remove schema-level entry from the bbf_schema_permissions catalog but skip the execution of revoke as object-level grants exist.
-		*/
+		/* 
+		 * Call the hook to add the permission in bbf_schema_permissions catalog 
+		 *  If the hook returns false, indicates that object-level and schema-level grants both are present and schema-level grant is revoked.
+		 *  In such case we remove schema-level entry from the bbf_schema_permissions catalog but skip the execution of revoke as object-level grants exist.
+		 */
 		if (update_bbf_schema_permissions_catalog_hook && !(*update_bbf_schema_permissions_catalog_hook) (this_privileges, istmt->is_grant, istmt->grantees,
 				istmt->col_privs, objectid, GetUserNameFromId(grantorId, false), 
 			istmt->grant_option, GetUserNameFromId(ownerId, false), istmt->objtype))
