7907264f6aac7448b5c2b6f44449c722d398876a
Rishabh Tanwar <ritanwar@amazon.com>
2026-01-05 11:08:47 +0000

    Fix pg_dump to dump Babelfish catalog data

--- src/bin/pg_dump/dump_babel_utils.c
+++ src/bin/pg_dump/dump_babel_utils.c
@@ -421,7 +421,7 @@ bbf_selectDumpableObject(DumpableObject *dobj, Archive *fout)
 							 * are not marked to be dumped so catalog table data explicitly
 							 * need to be marked as dumpable.
 							 */
-							if (isBabelfishConfigTable(fout, tbinfo) && !fout->dopt->dumpData)
+							if (isBabelfishConfigTable(fout, tbinfo) && fout->dopt->dumpSchema)
 							{
 								tbinfo->dobj.dump |= DUMP_COMPONENT_DATA;
 								/* also prepare the dumpable object upfront */
